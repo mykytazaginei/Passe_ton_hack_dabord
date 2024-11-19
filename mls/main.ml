62070839b787c4b42923f854b509e9c61cfcf0ca
Dmytro Honcharenko <dimeeentor@gmail.com>
2024-11-19 11:17:28 +0100

    fix for parse_lines_to_users

--- mls/main.ml
+++ mls/main.ml
@@ -3,17 +3,17 @@
 type user_info = { login : string; password : string };;
 type data_file = user_info list;;
 
-let lines: (string*string) list = read_data_from_file "tools/depensetout01.txt";;
+let lines: (string * string) list = read_data_from_file "tools/test.txt";;
   
-let rec parse_lines_to_users (lines: (string*string) list): data_file =
+let rec parse_lines_to_users (lines: (string * string) list) : data_file =
   if lines = [] then
     []
   else if List.length lines < 2 then
     failwith "ffffeffq"
   else
-    let login = List.hd lines in
+    let login = fst (List.hd lines) in
     let tail = List.tl lines in
-    let password = List.hd tail in
+    let password = snd (List.hd lines) in
     let remaining = List.tl tail in
     { login; password } :: parse_lines_to_users remaining
 ;;

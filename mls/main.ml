1aab3fad9a9e97b7260902e0fa2ede04054a2fbb
Mykyta Zaginei <nikitazaginey1@gmail.com>
2024-11-19 11:37:57 +0100

    fix is dupa

--- mls/main.ml
+++ mls/main.ml
@@ -3,25 +3,25 @@
 type user_info = { login : string; password : string };;
 type data_file = user_info list;;
 
-let rec is_duplicate (pair: string * string) (lst: (string * string) list) : bool =
+let rec is_duplicate_login (login: string) (lst: (string * string) list) : bool =
   if lst = [] then false
-  else 
-    let (login, password) = pair in
-    let (curr_login, curr_password) = List.hd lst in
-    if login = curr_login && password = curr_password then true
-    else is_duplicate pair (List.tl lst)
+  else
+    let (curr_login, _) = List.hd lst in
+    if login = curr_login then true
+    else is_duplicate_login login (List.tl lst)
 ;;
 
-let rec remove_duplicates (pairs: (string * string) list) : (string * string) list =
+let rec remove_duplicates_by_login (pairs: (string * string) list) : (string * string) list =
   if pairs = [] then []
   else
-    let curr_pair = List.hd pairs in
+    let (curr_login, curr_password) = List.hd pairs in
     let rest = List.tl pairs in
-    if is_duplicate curr_pair rest then remove_duplicates rest
-    else curr_pair :: remove_duplicates rest
+    if is_duplicate_login curr_login rest then remove_duplicates_by_login rest
+    else (curr_login, curr_password) :: remove_duplicates_by_login rest
 ;;
 
-let lines: (string * string) list = remove_duplicates (read_data_from_file "tools/test.txt");;
+
+(*let lines: (string * string) list = remove_duplicates (read_data_from_file "tools/test.txt");;*)
 
 (* Main function to call *)
 let rec parse_lines_to_users (lines: (string * string) list) : data_file =
@@ -37,3 +37,5 @@ let read_and_parse_file filename =
   let lines = read_data_from_file filename in
   parse_lines_to_users lines
 ;;
+
+read_and_parse_file "tools/test.txt"

a28518465f7dc5bde47c1c86e5c4645e8380a9fc
Mykyta Zaginei <nikitazaginey1@gmail.com>
2024-11-19 11:12:44 +0100

    jopa

--- mls/main.ml
+++ mls/main.ml
@@ -1,11 +1,21 @@
-#use "tools.ml";;
-
-let hash = hash_password "password123";;
+#use "tools/tools.ml";;
 
 type user_info = { login : string; password : string };;
 type data_file = user_info list;;
 
-let lines: string*string list = read_data_from_file "tools/depensetout01.txt";;
-
-let split_data(lines: string list): data_file = 
+let lines: (string*string) list = read_data_from_file "tools/depensetout01.txt";;
   
+let rec parse_lines_to_users (lines: (string*string) list): data_file =
+  if lines = [] then
+    []
+  else if List.length lines < 2 then
+    failwith "ffffeffq"
+  else
+    let login = List.hd lines in
+    let tail = List.tl lines in
+    let password = List.hd tail in
+    let remaining = List.tl tail in
+    { login; password } :: parse_lines_to_users remaining
+;;
+
+parse_lines_to_users(lines);;
